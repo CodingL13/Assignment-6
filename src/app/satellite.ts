b4a9b8b94a67de577293d2f146050a53303ed6a7
CodingL13 <lorechaser3@gmail.com>
2022-03-31 12:05:43 -0400

    Update satellite.ts

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -1,22 +1,26 @@
 export class Satellite {
-    name: string;
-    orbitType: string;
-    type: string;
-    operational: boolean;
-    launchDate: string;
 
-    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
-        this.name = name;
-        this.type = type;
-        this.operational = operational;
-        this.orbitType = orbitType;
-        this.launchDate = launchDate;
-    }
-    shouldShowWarning():boolean{
-        if (this.type.toLowerCase() === 'space debris'){
-            return true;
-        } else {
-            return false;
-        }
-    }
+	name: string;
+	type: string;
+	launchDate: string;
+	orbitType: string;
+	operational: boolean;
+
+	constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
+		this.name = name;
+		this.type = type;
+		this.launchDate = launchDate;
+		this.orbitType = orbitType;
+		this.operational = operational;
+	}
+
+	isSpaceDebris(): boolean {
+		if (this.type === "Space Debris") {
+			return true;
+		} 
+			return false;
+	}
+
 }
+
+// TODO 3a: fix isSpaceDebris check
